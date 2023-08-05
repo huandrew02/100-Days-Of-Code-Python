f4d7ce4dd7c074f364bb07023a0c8906cacf1491
Andrew <huandrew321@gmail.com>
2023-08-05 18:29:54 -0400

    Finished Day 30, Errors and Exceptions

--- 100DayCourse/Day29/main.py
+++ 100DayCourse/Day29/main.py
@@ -2,6 +2,26 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+    if len(website_entry.get()) == 0:
+        messagebox.showerror(title="No Entry", message="Please go back and enter a website.")
+    else:
+        try:
+            with open("100DayCourse/Day29/data.json", "r") as data_file:
+                # Reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            messagebox.showerror(title="No Data File", message="No data file found.")
+        else:
+            if website_entry.get() in data:
+                email = data[website_entry.get()]["email"]
+                password = data[website_entry.get()]["password"]
+                messagebox.showinfo(title=website_entry.get(), message=f"Email: {email}\nPassword: {password}")
+            else:
+                messagebox.showerror(title="No Data", message=f"No details for {website_entry.get()} exists.")
+    
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def aipass():
     password_entry.delete(0, END)
@@ -29,15 +49,36 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password
+        }
+    }
 
-    if website or email or password:
+    if len(website) == 0 or len(password) == 0:
         messagebox.showerror(title="No Entry", message="Please go back and fill out the blanks")
     else:
+
+
         is_ok = messagebox.askokcancel(title=website, message=f"These are the details: \nEmail: {email}\nPassword: {password}\n\nIs this correct?")
 
         if is_ok:
-            with open("100DayCourse/Day29/data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
+            try:
+                with open("100DayCourse/Day29/data.json", "r") as data_file:
+                    # Reading old data
+                    data = json.load(data_file)
+            except FileNotFoundError:
+                with open("100DayCourse/Day29/data.json", "w") as data_file:
+                    json.dump(new_data, data_file, indent=4)
+            else:
+                # Updating old data with new data
+                data.update(new_data)
+
+                with open("100DayCourse/Day29/data.json", "w") as data_file:
+                    # Saving updated data
+                    json.dump(data, data_file, indent=4)
+            finally:
                 website_entry.delete(0, END)
                 password_entry.delete(0, END)
 # ---------------------------- UI SETUP ------------------------------- #
@@ -56,7 +97,7 @@ website_label.grid(row=1, column=0)
 
 website_entry = Entry()
 website_entry.focus()
-website_entry.grid(row=1, column=1, columnspan=2, sticky="EW")
+website_entry.grid(row=1, column=1, columnspan=2, sticky="W")
 
 email_label = Label(text="Email/Username:")
 email_label.grid(row=2, column=0)
@@ -69,13 +110,16 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 password_entry = Entry()
-password_entry.grid(row=3, column=1, sticky="EW")
+password_entry.grid(row=3, column=1, sticky="W")
 
 generate_button = Button(text="Generate Password", command=aipass)
 generate_button.grid(row=3, column=2)
 
 add_button = Button(text="Add", command=save)
 add_button.grid(row=4, column=1, columnspan=2, sticky="EW")
+
+search_button = Button(text="Search", command=find_password)
+search_button.grid(row=1, column=2, sticky="EW")
 
 
 window.mainloop()
